0b18adf76a8bef5213ac212c1202f546d4db6108
Lee-d0 <marmar11137@gmail.com>
2024-09-18 13:57:26 -0400

    sendTextMsg created

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -16,6 +16,7 @@ import type {
 import type * as clerk from "../clerk.js";
 import type * as conversation from "../conversation.js";
 import type * as http from "../http.js";
+import type * as messages from "../messages.js";
 import type * as users from "../users.js";
 
 /**
@@ -30,6 +31,7 @@ declare const fullApi: ApiFromModules<{
   clerk: typeof clerk;
   conversation: typeof conversation;
   http: typeof http;
+  messages: typeof messages;
   users: typeof users;
 }>;
 export declare const api: FilterApi<

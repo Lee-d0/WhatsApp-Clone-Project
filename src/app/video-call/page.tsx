4c3a68631711a6d796ad1b03c37a3fecfbdc43af
Lee-d0 <marmar11137@gmail.com>
2024-09-26 21:21:46 -0400

    update2

--- src/app/video-call/page.tsx
+++ src/app/video-call/page.tsx
@@ -3,7 +3,6 @@ import dynamic from "next/dynamic";
 
 const DynamicVideoUI = dynamic(() => import("./video-ui-kit"), { ssr: false });
 
-
 export default function VideoCall() {
 	return <DynamicVideoUI />;
 }
